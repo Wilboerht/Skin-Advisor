1be77aaaf5bf88f021038886ea2d117e0a4efa47
Hank <hongkunw2002@gmail.com>
2026-03-20 16:18:50 +0800

    1

--- src/components/website/WebsiteLayoutClient.tsx
+++ src/components/website/WebsiteLayoutClient.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 import { LayoutProvider } from "@/contexts/LayoutContext";
 import { KineticBackground } from "@/components/website/KineticBackground";
 import { BentoBackground } from "@/components/website/BentoBackground";
-import { BottomNavBar } from "@/components/website/BottomNavBar";
 
 /**
  * WebsiteLayoutClient
@@ -13,8 +12,7 @@ import { BottomNavBar } from "@/components/website/BottomNavBar";
  * 1. 提供 LayoutContext (管理抽屉状态)
  * 2. 渲染全局单一的 KineticBackground
  * 3. 增加 BentoBackground (抽屉收起时显示)
- * 4. 增加全局单一的 BottomNavBar (复刻 nihplod.cn 版)
- * 5. 包装页面内容
+ * 4. 包装页面内容
  */
 export function WebsiteLayoutClient({ children }: { children: ReactNode }) {
     return (
@@ -24,9 +22,6 @@ export function WebsiteLayoutClient({ children }: { children: ReactNode }) {
 
             {/* Bento Grid 背景 (仅在收起时显示) */}
             <BentoBackground />
-
-            {/* 全局底部导航栏 (Dock 样式) */}
-            <BottomNavBar />
 
             {/* 布局 Provider */}
             {children}
