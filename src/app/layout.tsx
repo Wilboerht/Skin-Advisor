37f10b91f593ca7d202ec258e4d3c679b4e3bafa
Hank <hongkunw2002@gmail.com>
2026-04-22 17:08:57 +0800

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
-      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
   manifest: '/site.webmanifest',
