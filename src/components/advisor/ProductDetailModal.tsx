afcb18e24ee564aa4bd718b55062ea946fd3a6b6
Hank <hongkunw2002@gmail.com>
2026-04-29 20:51:30 +0800

    1

--- src/components/advisor/ProductDetailModal.tsx
+++ src/components/advisor/ProductDetailModal.tsx
@@ -38,9 +38,17 @@ export function ProductDetailModal({ isOpen, onClose, product }: ProductDetailMo
 
     useEffect(() => {
         if (isOpen) {
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+            document.body.style.overflow = "hidden";
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
             document.addEventListener("keydown", handleKeyDown);
+        } else {
+            document.body.style.overflow = "";
+            document.body.style.paddingRight = "";
         }
         return () => {
+            document.body.style.overflow = "";
+            document.body.style.paddingRight = "";
             document.removeEventListener("keydown", handleKeyDown);
         };
     }, [isOpen, handleKeyDown]);
