976a715da08628ed13c0d3f55a67fa98afb299b1
Hank <hongkunw2002@gmail.com>
2026-05-17 22:35:52 +0800

    1

--- src/components/advisor/ReportCards.tsx
+++ src/components/advisor/ReportCards.tsx
@@ -204,8 +204,8 @@ export default function ReportCards({
           </div>
 
           {/* Decorative Image Area (Right) */}
-          <div className="absolute right-[20px] lg:-right-33 bottom-[105px] lg:-bottom-6 w-1/2 h-[110%] pointer-events-none flex items-end justify-end z-20">
-            <div className="absolute -bottom-[53%] lg:-bottom-[30%] w-[16rem] sm:w-[28rem] h-[16rem] sm:h-[28rem] z-30 drop-shadow-2xl right-[-10px] lg:right-[-48px]">
+          <div className="absolute right-[-27px] lg:-right-33 bottom-[113px] lg:-bottom-6 w-1/2 h-[110%] pointer-events-none flex items-end justify-end z-20">
+            <div className="absolute -bottom-[53%] lg:-bottom-[30%] w-[16rem] sm:w-[28rem] h-[16rem] sm:h-[28rem] z-30 drop-shadow-2xl right-[-55px] lg:right-[-48px]">
               <Image
                 src="/images/gift.webp"
                 alt="礼盒"
