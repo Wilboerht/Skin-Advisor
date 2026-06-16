7b704983dda1ce0cfcdb3333e9e02098b3af1da1
Hank <hongkunw2002@gmail.com>
2026-06-16 16:46:26 +0800

    1

--- src/app/skin-types/page.tsx
+++ src/app/skin-types/page.tsx
@@ -48,7 +48,7 @@ export default function ResultIndexPage() {
 
       {/* 类型卡片 */}
       <section className="pb-24 md:pb-32 px-6 md:px-12 lg:px-20">
-        <div className="max-w-5xl mx-auto grid md:grid-cols-2 gap-5">
+        <div className="max-w-5xl mx-auto grid md:grid-cols-2 gap-6">
           {orderedTypes.map((type, i) => {
             if (!type) return null;
             return (
