11e2b6f92ea09a23d34d4736a541c595f0e867f5
Hank <hongkunw2002@gmail.com>
2026-07-30 02:25:40 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -500,7 +500,7 @@ export default function Home() {
                           ) : (
                             <>
                               <ScanFace className="w-4 h-4" />
-                              <span>面部扫描测肤</span>
+                              <span>开始 AI 面部扫描</span>
                               <ArrowRight className="w-4 h-4 transition-transform duration-500 group-hover:translate-x-1" />
                             </>
                           )}
